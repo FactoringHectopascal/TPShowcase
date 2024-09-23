0139f1eb1d598221d4bd68450a90ed9c1ef864b4
FactoringHectopascal <26.caleb.guzman@ksd.org>
2024-09-23 10:28:09 -0700

    Finishing Touches, Laying Baseline for Home.

---
+++ Top Down Showcase/Assets/Scripts/EnemyCount.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCount : MonoBehaviour
+{
+    //https://docs.unity3d.com/ScriptReference/GameObject.Find.html Get Started HERE Friend!
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
