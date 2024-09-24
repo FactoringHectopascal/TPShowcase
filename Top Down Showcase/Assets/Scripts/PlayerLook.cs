33ba4ed03578102277865ea689f92402de59f178
FactoringHectopascal <26.caleb.guzman@ksd.org>
2024-09-24 09:30:35 -0700

    Animations, Baseline for a new script

---
+++ Top Down Showcase/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    //DOCUMENTATION FOR THIS: https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Transform-position.html
+    // https://docs.unity3d.com/ScriptReference/Input-mousePosition.html
+    // https://docs.unity3d.com/ScriptReference/Transform.LookAt.html
+    // https://docs.unity3d.com/ScriptReference/Camera.ScreenToWorldPoint.html
+    // https://docs.unity3d.com/ScriptReference/Vector3-up.html (Literally just a shorthand)
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
