979412f8728464c2fb1f0e85ac748a5c5e82be62
FactoringHectopascal <26.caleb.guzman@ksd.org>
2024-09-24 20:49:35 -0700

    Added Notes to my scripts

--- Top Down Showcase/Assets/Scripts/CameraFollow.cs
+++ Top Down Showcase/Assets/Scripts/CameraFollow.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
+    //allows me to set the transform target (so I can choose what I want the camera to follow)
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        //Sets the transform position to the targets x and y values, while still retaining the z values
+        //so that it does not clip through the floor.
     }
 }
--- Top Down Showcase/Assets/Scripts/PlayerLook.cs
+++ Top Down Showcase/Assets/Scripts/PlayerLook.cs
@@ -11,18 +11,23 @@ public class PlayerLook : MonoBehaviour
     // https://docs.unity3d.com/ScriptReference/Transform.LookAt.html
     // https://docs.unity3d.com/ScriptReference/Camera.ScreenToWorldPoint.html
     // https://docs.unity3d.com/ScriptReference/Vector3-up.html (Literally just a shorthand)
+    //DOES NOT WORK IF THE CAMERA IS A CHILD OF THE PLAYER GAMEOBJECT, USE THIS IN CONJUCTION
+    //WITH A CAMERA FOLLOW SCRIPT
 
     void Update()
     {
         if (Time.timeScale >= 1)
         {
             FaceMouseDir();
+            //if the timescale isn't 0 or <0, allow me to run this function
         }
 
     }
     public void FaceMouseDir()
     {
         Vector2 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //converts the mouses position to worldspace, instead of screenspace.
         transform.up = mousepos - new Vector2(transform.position.x, transform.position.y);
+        //moves the game objects rotation and y axis according to the mouse position - transform.x & y
     }
 }
