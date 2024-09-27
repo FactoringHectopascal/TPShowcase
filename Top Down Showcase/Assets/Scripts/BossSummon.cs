f2b3bcc5326484dd852ce3ff532678376e0de515
FactoringHectopascal <26.caleb.guzman@ksd.org>
2024-09-27 10:16:09 -0700

    Added Summoning Script, Kind Of Trash, fix later?

---
+++ Top Down Showcase/Assets/Scripts/BossSummon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BossSummon : MonoBehaviour
+{
+    
+    [SerializeField]
+    GameObject minion;
+    [SerializeField]
+    bool canSpawn = false;
+    //DOCUMENTATION: https://docs.unity3d.com/ScriptReference/MonoBehaviour.StartCoroutine.html
+    // https://docs.unity3d.com/ScriptReference/WaitForSeconds.html
+    // https://docs.unity3d.com/Manual/Coroutines.html
+    // https://docs.unity3d.com/ScriptReference/Coroutine.html
+    void Start()
+    {
+        if (Time.time > 1)
+        {
+            StartCoroutine(Spawn());
+        }
+    }
+
+    public IEnumerator Spawn()
+    {
+        canSpawn = true;
+        Spawnin();
+        yield return new WaitForSeconds(2f);
+    }
+    public void Spawnin()
+    {
+        if (canSpawn == true)
+        {
+            Instantiate(minion, transform.position, Quaternion.identity);
+            canSpawn = false;
+        }
+    }
+    void Update()
+    {
+      
+    }
+}
